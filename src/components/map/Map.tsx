559a5ef973f4145e216ef5a036f4600177032a23
James Byrne <mail@jamestbyrne.com>
2021-03-20 14:49:49 +0000

    Initial setup

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+const Map = () => {
+  const [viewport, setViewport] = useState("");
+
+  return <div></div>;
+};
+
+export default Map;
